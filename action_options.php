76183c036d5c10a36f4fad6ba29f1eb0c59e8a23
pfrancois (portable) <pfrancois_99@yahoo.fr>
2010-11-17 08:54:26 +0100

    continuation

--- action_options.php
+++ action_options.php
@@ -65,43 +65,13 @@ if ($action=="effacer_tiers_vides"){
 		exit();
 	}
 }
-//-----------------------------change les dates des retraits
 
-//---------------------------change les date des operations cartes differes
-if ($action=="dates_ope_diff"){
-	function callback($iter){
-		//fait ce qui est demandé
-		//attention, elle peut changer
-		try {
-//			var_dump($iter);
-			$notes=$iter->get_notes();
-			if ($notes!=''){
-				if (preg_match('#CARTE X9438 (../..)#', $notes,$n)){
-					$date_a_verifier=util::datefr2time($n[1]."/".(idate("Y")));
-					//verifie si on ne met pas l'annee suivante
-					if ($date_a_verifier>time()){
-						$date_a_verifier=util::datefr2time($n[1]."/".(idate("Y")-1));
-					}
-					//ral($iter->get_date().":".$date_a_verifier);
-					if ($iter->get_date()!=$date_a_verifier){
-						$iter->set_date($date_a_verifier);
-						$nouvelle_note=str_replace("CARTE X9438","CB SG",$notes);
-						$iter->set_notes($nouvelle_note);
-						ral("operation ".$iter->get_id()." du ".$iter->get_date()." pour ".(util::cent2fr($iter->get_montant(),2)));
-						return 1;
-					}
-				}
-			}
-			 throw new Exception_no_reponse('pas de reponse');
-		} catch (Exception_no_reponse $e) {
-			return 0;
-		}
-	}
+//---------------------------verifications des differents totaux
+if ($action=="verif_totaux"){
 	///// normalement, pas besoin de changer en dessous
 	if ($phase==""){
+		$tpl->assign('titre','verification des totaux sur '.$gsb_xml->get_xmlfile());
 		$i=0;
-		$tpl->assign('titre','liste des opérations modifies sur '.$gsb_xml->get_xmlfile());
-		//remplissez la requete xpath
 		try {
 			foreach ($gsb_operations->iter() as $iter) {
 				$i=$i+callback($iter);
@@ -123,7 +93,70 @@ if ($action=="dates_ope_diff"){
 			$i=$i+callback($iter);
 		}
 		$gsb_xml->save();
-		ral("$i tiers effac&eacutes");
+		ral("$i opérations modifiées");
+		$tpl->assign("nom_classe_css","ligne");
+		$tpl->assign("lien","options.php");
+		$tpl->display('resultats.smarty');
+		exit();
+	}
+}
+
+//---------------------------change les date des operations cartes differes
+if ($action=="dates_ope_diff"){
+	function callback($iter){
+		try {
+			$notes=$iter->get_notes();
+			if ($notes!=''){
+				if (preg_match('#CARTE X9438 (../..)#', $notes,$n)){
+					$date_a_verifier=util::datefr2time($n[1]."/".(idate("Y")));
+					//verifie si on ne met pas l'annee suivante
+					if ($date_a_verifier>time()){
+						$date_a_verifier=util::datefr2time($n[1]."/".(idate("Y")-1));
+					}
+					if ($iter->get_date()!=$date_a_verifier){
+						$iter->set_date($date_a_verifier);
+						$nouvelle_note=str_replace("CARTE X9438","CB SG",$notes);
+						$iter->set_notes($nouvelle_note);
+						ral("operation ".$iter->get_id()." du ".$iter->get_date()." pour ".(util::cent2fr($iter->get_montant(),2)));
+						return 1;
+					}
+				}
+			}
+			 throw new Exception_no_reponse('pas de reponse');
+		} catch (Exception_no_reponse $e) {
+			return 0;
+		}
+	}
+	///// normalement, pas besoin de changer en dessous
+	if ($phase==""){
+		$i=0;
+		$tpl->assign('titre','liste des opérations modifies sur '.$gsb_xml->get_xmlfile());
+		try {
+			foreach ($gsb_operations->iter() as $iter) {
+				$i=$i+callback($iter);
+			}
+			ral("$i opérations à modifier");;
+		} catch (Exception_no_reponse $e) {
+			ral("aucune actions effectué car &laquo;$xpath&raquo; non trouvé");
+		}
+		$tpl->assign("nom_classe_css","ligne");
+		$tpl->assign("lien","action_outils.php?action=dates_ope_diff&amp;phase=2");
+		$tpl->display('resultats.smarty');
+		exit();
+	}
+	if ($phase==2){
+		$i=0;
+		ral('liste des actions effectues sur '.$gsb_xml->get_xmlfile());
+		try {
+			foreach ($gsb_operations->iter() as $iter) {
+				$i=$i+callback($iter);
+			}
+			ral("$i opérations modifiées");;
+		} catch (Exception_no_reponse $e) {
+			ral("aucune actions effectué car &laquo;$xpath&raquo; non trouvé");
+		}
+		$gsb_xml->save();
+		ral("$i opérations modifiées");
 		$tpl->assign("nom_classe_css","ligne");
 		$tpl->assign("lien","options.php");
 		$tpl->display('resultats.smarty');
