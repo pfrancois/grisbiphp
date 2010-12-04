2cda34d256da6c43174671e1f9d13cbfb167408c
pfrancois(fixe) <pfrancois_99@yahoo.fr>
2010-12-04 14:06:35 +0100

    filtre des operation ventiles mere dans l'export csv

--- export_csv.php
+++ export_csv.php
@@ -9,7 +9,7 @@ foreach ($gsb_operations->iter() as $ope) {
 		$cat="virement";
 	} else {
 		if ($ope->is_ventilee()) {
-			$cat="ovm";
+			continue;// on  ne prend pas les operation meres des ventilees
 		}else {
 			if (is_null($ope->get_categorie())){
 				$cat="";
@@ -22,26 +22,26 @@ foreach ($gsb_operations->iter() as $ope) {
 		}
 	}
 	$csv[$ope->get_id()]=array(
-	$ope->get_id(),
-	utf8_decode ($ope->get_compte()->get_nom()),
-	date("d/m/Y",$ope->get_date()),
-	util::cent2fr($ope->get_montant()),
-	($ope->get_statut_pointage() == rapp::RAPPROCHEE)?1:0,
-	($ope->get_statut_pointage() == rapp::POINTEE)?1:0,
-	(is_null($ope->get_moyen()))?"":utf8_decode ($ope->get_moyen()->get_nom()),
-	utf8_decode ($cat),
-	utf8_decode ($ope->get_tiers()->get_nom()),
-	utf8_decode ($ope->get_notes()),
-	(is_null($ope->get_ib()))?"":utf8_decode ($ope->get_ib()->get_nom()),
-	$ope->get_num_chq(),
-	($ope->is_virement())?$ope->get_operation_contrepartie()->get_id():0,
-	($ope->is_ventilation())?$ope->get_operation_mere():0
+		$ope->get_id(),
+		utf8_decode ($ope->get_compte()->get_nom()),
+		date("d/m/Y",$ope->get_date()),
+		util::cent2fr($ope->get_montant()),
+		($ope->get_statut_pointage() == rapp::RAPPROCHEE)?1:0,
+		($ope->get_statut_pointage() == rapp::POINTEE)?1:0,
+		(is_null($ope->get_moyen()))?"":utf8_decode ($ope->get_moyen()->get_nom()),
+		utf8_decode ($cat),
+		utf8_decode ($ope->get_tiers()->get_nom()),
+		utf8_decode ($ope->get_notes()),
+		(is_null($ope->get_ib()))?"":utf8_decode ($ope->get_ib()->get_nom()),
+		$ope->get_num_chq(),
+		($ope->is_virement())?$ope->get_operation_contrepartie()->get_id():0,
+		($ope->is_ventilation())?"vrai":"faux"
 	);
 }
 //creation du csv
 $file='comptes.csv';
 $fp = fopen($file, 'w');
-fputcsv($fp, array("id","nom_compte","date","montant","R","P","moyen","categorie","tiers","notes","projet","chequ","virement","ovm"),';','"');
+fputcsv($fp, array("id","nom_compte","date","montant","R","P","moyen","categorie","tiers","notes","projet","cheque","virement","ventilation"),';','"');
 foreach ($csv as $fields) {
 	fputcsv($fp, $fields,';','"');
 }
